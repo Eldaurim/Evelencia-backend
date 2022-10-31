1698c366530e627ac33a89e7d4a0b30cbfb86ea7
Samuel Bacconin <samuel.bacconin@live.fr>
2022-10-31 14:11:44 +0100

    api folder

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authenticationRoutes = require('./routes/authentication');
-const userRoutes = require('./routes/user');''
+const authenticationRoutes = require('./api/routes/authentication');
+const userRoutes = require('./api/routes/user');''
 
 const app = express();
 app.use(express.json());
